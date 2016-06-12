3a8ad4ea0a96b208b7d52d0dc7ee355cf297072c
SagarPi <11king.sagar@gmail.com>
2016-06-12 21:21:21 +0530

    Express Added

---
+++ Server/SimpleServer.js
@@ -0,0 +1,12 @@
+
+
+// var express = require('express');
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'content-type': 'text/plain'});
+    res.end('Welcome to Sagar Pi');
+}).listen(1337, '127..0.0.1');
+
+console.log('Server running on http://127.0.0.1:1337/');
