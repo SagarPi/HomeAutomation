a2866c7e7cc5905fed4344df8f04b767e9c3dfb0
Mate Sagar <mateesag@ap.dtcs.local>
2016-06-13 17:56:28 +0530

    practice

--- Server/SimpleExpressServer.js
+++ Server/SimpleExpressServer.js
@@ -4,10 +4,13 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'js');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/Sagar', function (req, res) {
     res.send('hello, Sagar Here');
 });
 
-app.listen(1337, function (params) {
+app.listen(1337, function () {
     console.log('ready on port 1337');
 });
