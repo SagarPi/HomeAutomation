0c389f943185425dff0d051de0149979551ebf5d
SagarPi <11king.sagar@gmail.com>
2017-02-26 20:04:53 +0530

    Update ClientControlledLEDService.js
    added logic for Led off

--- Server/ClientControlledLEDService.js
+++ Server/ClientControlledLEDService.js
@@ -3,39 +3,39 @@ var rpio = require('rpio');
 var app=express();
 
 app.get('/switch',function (req,res) {
-//if(req.query.val==true)
+    console.log('inside on');
+    rpio.open(12, rpio.OUTPUT, rpio.LOW);
+    console.log('set the output pin');
+
+    if(req.query.val=='true')
     On();
+else
+    Off();
 res.send("should switch On : " + req.query.val);
 })
 
 
 
 function On() {
-
-    console.log('inside on');
-    rpio.open(12, rpio.OUTPUT, rpio.LOW);
-    // rpio.mode(12, rpio.OUTPUT);
-    console.log('set the output pin');
-    if(rpio.read(12) == rpio.LOW)
-        console.log("LED is OFF");
-    else
-        console.log("LED is ON");
-
-      //  if(rpio.read(12) == rpio.LOW)
+    check();
             rpio.write(12, rpio.HIGH);
     console.log('pin value is high');
+check();
+}
 
+
+function Off() {
+    check();
+    rpio.write(12, rpio.HIGH);
+    console.log('pin value is high');
+    check();
+}
+
+function check() {
     if(rpio.read(12) == rpio.LOW)
         console.log("LED is OFF");
     else
         console.log("LED is ON");
-
-    //  else
-        //    console.log('LED already ON');
-        //console.log('Value of Pin3 from Read Pin is ' + rpio.read(12));
-
-
-
 }
 
 
